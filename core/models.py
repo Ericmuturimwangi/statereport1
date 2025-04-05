6f12c67b112b1ebfddfb5024f51d1485e3ba839c
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-04-05 23:09:17 +0300

    added db models

---
+++ core/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class HQ(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__ (self):
+        return self.user.username
+    
+class Brigade(models.Model):
+    name = models.CharField(max_length=100)
+    hq = models.ForeignKey(HQ, on_delete=models.CASCADE)
+    account = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __Str__(self):
+        return self.name
+    
+class Unit(models.Model):
+    name = models.CharField(max_length=100)
+    brigade = models.ForeignKey(Brigade, on_delete=models.CASCADE)
+
+    def __Str__(self):
+        return self.name
+    
+class PDFSubmission(models.Model):
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    date = models.DateField()
+    file = models.FileField(upload_to='unit_pdfs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Submission by {self.unit.name} on {self.date}"
+    
+class BrigadeReport(models.Model):
+    brigade = models.ForeignKey(Brigade, on_delete=models.CASCADE)
+    date = models.DateField()
+    compiled_file = models.FileField(upload_to='brigade_reports/')
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Report for {self.brigade.name} on {self.date}"
+
