feb071c20d9834856186f1d6fdb11870a6cddfb7
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-04-20 23:05:57 +0300

    bde

--- sitrep/settings.py
+++ sitrep/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,3 +138,5 @@ CACHES ={
         "LOCATION": "redis://127.0.0.1:6379/1",
     }
 }
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
